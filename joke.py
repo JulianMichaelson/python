0c03e36c331c6c6afdd7211dd7d69792d39ad2f5
JulianMichaelson <julianmichaelson06@gmail.com>
2026-08-26 10:08:15 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,4 @@
+# Assignment 1B: Input & Output, My First Program, Joke Program
+print("A man was seated at a bar, he had drank 12 shots. After his 8th shot a horse walks into the bar. The man starts to conversate with the horse, People around him look at him as if he's crazy, why is that? ")
+print("Cause horses can't speak.")
+print("And he was drunk.")
