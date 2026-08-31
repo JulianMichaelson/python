901765225c28ed8fe8a4ec491b546df56924ce83
JulianMichaelson <julianmichaelson06@gmail.com>
2026-08-31 13:43:44 -0500

    data.py RPG Assignment
    Added RPG character profile and item details with formatted print statements.

--- data.py
+++ data.py
@@ -1,3 +1,17 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
 print("double quotes")
 print('single quotes')
 
@@ -10,7 +24,6 @@ my_int = "1"
 # These values look similar, but integer is a number and my_int is text.
 # print("Adding number to a string:")
 # print(integer + my_int)
-
 
 print("Adding strings: ")
 print(my_int + my_int)
@@ -30,3 +43,45 @@ print(coffee)
 # This block runs because tired is True.
 if tired:
         print("I need coffee")
+
+--------------------------------------------------------------------------------------------------------------------------
+# RPG Character Profile Assignment Portion
+character_name = "Julian"
+height = "6'0"
+weight = 160
+skin_color = "Tan"
+
+print(f"Name: {character_name}")
+print(f"Height: {height}")
+print(f"Weight: {weight}")
+print(f"Skin Tone: {skin_color}")
+
+item_name = "Orbs of Healing"
+quantity = 5
+price = 30.25
+orbs_on_sale = True
+
+print(f"Item: {item_name}")
+print(f"Quantity: {quantity}")
+print(f"{item_name} cost {character_name} {price} for 1, {character_name} has {quantity}. Costing him a total of $151.25")
+print(f"On Sale: {orbs_on_sale}")
+
+weapon_type_and_name = "Sword of Falabath"
+damage = 45
+sword_price = 1000
+sword_on_sale = False
+
+print(f"Weapon: {weapon_type_and_name}")
+print(f"Damage: {damage}")
+print(f"Price: {sword_price}")
+print(f"On Sale: {sword_on_sale}")
+
+armor_name = "Armor of Falabath"
+protection = 85
+armor_price = 1500
+armor_on_sale = False
+
+print(f"Armor: {armor_name}")
+print(f"Protection: {protection}")
+print(f"Price: {armor_price}")
+print(f"On Sale: {armor_on_sale}")
